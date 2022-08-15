aa8ec4925dc5a18a878235773cdb87a0bf061707
Vladimir Okun <Vladimir@okun.pro>
2022-08-15 10:21:15 +0300

    * add server1\server2 domain name to hosts file with its target ip to allow domain name resolution * edit `PasswordAuthentication` to allow using default vagrant password on ssh login between the boxes * restart secure shell service to apply aforementioned password setting.

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+sudo /bin/sh -c 'echo "192.168.60.10 server1" >> /etc/hosts'
+/usr/bin/sed -i 's:PasswordAuthentication no:PasswordAuthentication yes:g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
