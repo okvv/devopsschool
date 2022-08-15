c76e759fda11e836c31589b70d41444e9b8eaab5
Vladimir Okun <Vladimir@okun.pro>
2022-08-15 10:25:06 +0300

    append, to ssh config, connection settings for shh to server1\server2. add box ip, user, and key path.

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo tee -a /home/vagrant/.ssh/config > /dev/null <<EOT
+Host server2
+  HostName 192.168.60.11
+  User vagrant
+  IdentityFile /vagrant/.vagrant/machines/server2/virtualbox/private_key
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOT
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo tee -a /home/vagrant/.ssh/config > /dev/null <<EOT
+Host server1
+  HostName 192.168.60.10
+  User vagrant
+  IdentityFile /vagrant/.vagrant/machines/server1/virtualbox/private_key
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOT
