73165103f875fe7e275e78bfc70207d7d2326910
Vladimir Okun <Vladimir@okun.pro>
2022-08-17 09:23:58 +0300

    A solution to question n. 6 * copy file to mirror box. * force scp using `/home/vagrant/.ssh/config` as tasked with making root user use script as well and cancel output. * print only file list size, easier than parsing scp output.

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+FILE_LIST=${*:1:$#-1}
+DESTINATION_FOLDER=${*: -1}
+[ "$HOSTNAME" = "server1" ] && DESTINATION_SERVER="server2" || DESTINATION_SERVER="server1"
+
+/usr/bin/scp -q -F /home/vagrant/.ssh/config $FILE_LIST $DESTINATION_SERVER:$DESTINATION_FOLDER
+/usr/bin/du -bcs $FILE_LIST | awk '{print $1}' | tail -1
